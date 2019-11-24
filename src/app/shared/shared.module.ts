d1f7d2ed74ba0eb00611df8fa611acd0607029bc
AlexM1105 <AlexM1105ur@gmail.com>
2019-11-25 00:08:52 +0200

    feat (shared): add material button/input modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,17 +3,27 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import {
+  MatButtonModule,
+  MatInputModule,
+} from '@angular/material';
 
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
+    // Material modules
+    MatButtonModule,
+    MatInputModule,
   ],
   exports: [
     ReactiveFormsModule,
     CommonModule,
     RouterModule,
+    // Material modules
+    MatButtonModule,
+    MatInputModule,
   ],
 })
 export class AppSharedModule { }
