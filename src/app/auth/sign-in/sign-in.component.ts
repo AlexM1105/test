f101ec905488a4ec86bf4b069c2a5825f7f69130
AlexM1105 <AlexM1105ur@gmail.com>
2019-11-25 00:36:16 +0200

    feat (auth:sign-in): add sign-in logic and base layout

--- src/app/auth/sign-in/sign-in.component.ts
+++ src/app/auth/sign-in/sign-in.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { validationErrors } from '../../shared/constants/validation-errors.const';
+import { ApplicationService } from '../../core/services/application.service';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrls: ['./sign-in.component.css'],
 })
 export class SignInComponent implements OnInit {
+  signInForm: FormGroup;
 
-  constructor() { }
+  errors = validationErrors;
 
-  ngOnInit() {
+  get nameControl(): AbstractControl {
+    return this.signInForm.get('name');
   }
 
+  get passwordControl(): AbstractControl {
+    return this.signInForm.get('password');
+  }
+
+  constructor(
+    private applicationService: ApplicationService,
+    private authService: AuthService,
+  ) {
+  }
+
+  ngOnInit() {
+    this.signInForm = new FormGroup({
+      name: new FormControl( '', [
+          Validators.required,
+          Validators.minLength(this.applicationService.validation.name.minlength),
+        ],
+      ),
+      password: new FormControl( '', [
+          Validators.required,
+          Validators.minLength(this.applicationService.validation.password.minlength),
+          Validators.maxLength(this.applicationService.validation.password.maxlength),
+          Validators.pattern(this.applicationService.validation.password.pattern),
+        ],
+      ),
+    });
+  }
+
+  onSubmit() {
+    if (this.signInForm.valid) {
+      this.authService.signIn(this.signInForm.value);
+    }
+  }
 }
