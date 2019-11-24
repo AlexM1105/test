e6f2820fd79e497ea4c4f79937fa8493ed736099
AlexM1105 <AlexM1105ur@gmail.com>
2019-11-24 18:07:19 +0200

    feat (shared): create shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule,
+  ],
+  exports: [
+    ReactiveFormsModule,
+    CommonModule,
+    RouterModule,
+  ],
+})
+export class AppSharedModule { }
