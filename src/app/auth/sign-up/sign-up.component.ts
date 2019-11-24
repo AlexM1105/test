3fd4709892913712df41e61decd96604dba2941b
AlexM1105 <AlexM1105ur@gmail.com>
2019-11-25 00:18:57 +0200

    feat (auth:sign-up): add form logic and base layout

--- src/app/auth/sign-up/sign-up.component.ts
+++ src/app/auth/sign-up/sign-up.component.ts
@@ -1,9 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { ApplicationService } from '../../core/services/application.service';
+import { AuthService } from '../../core/services/auth.service';
+import { validationErrors } from '../../shared/constants/validation-errors.const';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.css'],
 })
-export class SignUpComponent {
+export class SignUpComponent implements OnInit {
+
+  signUpForm: FormGroup;
+
+  errors = validationErrors;
+
+  get nameControl(): AbstractControl {
+    return this.signUpForm.get('name');
+  }
+
+  get passwordControl(): AbstractControl {
+    return this.signUpForm.get('password');
+  }
+
+  get confirmPasswordControl(): AbstractControl {
+    return this.signUpForm.get('confirmPassword');
+  }
+
+  constructor(
+    private applicationService: ApplicationService,
+    private authService: AuthService,
+  ) {
+  }
+
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      name: new FormControl('', [
+          Validators.required,
+          Validators.minLength(this.applicationService.validation.name.minlength),
+        ],
+      ),
+      password: new FormControl('', [
+          Validators.required,
+          Validators.minLength(this.applicationService.validation.password.minlength),
+          Validators.maxLength(this.applicationService.validation.password.maxlength),
+          Validators.pattern(this.applicationService.validation.password.pattern),
+        ],
+      ),
+      confirmPassword: new FormControl('', [
+          Validators.required,
+          Validators.minLength(this.applicationService.validation.password.minlength),
+          Validators.maxLength(this.applicationService.validation.password.maxlength),
+          Validators.pattern(this.applicationService.validation.password.pattern),
+        ],
+      ),
+    }, this.validateEqualPassword);
+
+  }
+
+  onSubmit() {
+    if (this.signUpForm.valid) {
+      this.authService.signUp(this.signUpForm.value);
+    }
+  }
+
+  validateEqualPassword(control: FormGroup) {
+    const pass = control.value.password;
+    const conf = control.value.confirmPassword;
+    if (pass !== conf) {
+      control.get('confirmPassword').setErrors({
+        ...control.get('confirmPassword').errors,
+        notEqual: true,
+      });
+    }
+    return null;
+  }
 }
