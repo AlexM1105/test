397e0a577d04a9675aac7826da11a69446eafe7b
AlexM1105 <AlexM1105ur@gmail.com>
2019-11-24 17:35:09 +0200

    feat (core:interceptors): create auth interceptor

---
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+
+import { SessionService } from '../services/session.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(
+    private sessionService: SessionService,
+  ) {
+  }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.sessionService.getToken();
+    if (token) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Token ${token}`
+        }
+      });
+    }
+    return next.handle(request);
+  }
+}
