1c46992cd7ea0c0e4f003a944bb216e6205e77ec
AlexM1105 <AlexM1105ur@gmail.com>
2019-11-26 23:20:26 +0200

    feat (main:products:products-list): add layout and styles

--- src/app/main/products/products-list/products-list.component.ts
+++ src/app/main/products/products-list/products-list.component.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
+import { ProductService } from '../../../core/services/product.service';
+import { ProductModel } from '../../../core/models/product.model';
+
 @Component({
   selector: 'app-products-list',
   templateUrl: './products-list.component.html',
   styleUrls: ['./products-list.component.scss'],
 })
 export class ProductsListComponent {
+
+  products$: Observable<ProductModel[]> = this.productService.products$;
+
+  constructor(
+    private productService: ProductService,
+  ) {
+  }
+
 }
