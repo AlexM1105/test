8982e399cb65715783a6d934078d29c66bcb4e47
AlexM1105 <AlexM1105ur@gmail.com>
2019-11-27 02:09:45 +0200

    feat (products:product-review): add reviews resolver

--- src/app/main/products/product-reviews/product-reviews-routing.module.ts
+++ src/app/main/products/product-reviews/product-reviews-routing.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ProductReviewsComponent } from './product-reviews.component';
+import { ReviewResolver } from '../../../core/resolvers/review.resolver';
 
 
 const routes: Routes = [
   {
     path: '',
     component: ProductReviewsComponent,
+    resolve: [
+      ReviewResolver,
+    ]
   }
 ];
 
