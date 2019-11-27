4830d2dad79ec846f82ab3ebdfc839754e7a005d
AlexM1105 <AlexM1105ur@gmail.com>
2019-11-27 22:15:04 +0200

    feat (main:components:header): add isGuest stream, hide sign-up/in links when logged in

--- src/app/main/components/header/header.component.ts
+++ src/app/main/components/header/header.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 
+import { Observable } from 'rxjs';
+
+import { AuthService } from '../../../core/services/auth.service';
+
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+
+  isGuest$: Observable<boolean> = this.authService.isGuest$;
+
+  constructor(
+    private authService: AuthService,
+  ) {
+  }
 }
