0a07f41f04804ee9a4ccaff9b08069c6f088fa38
AlexM1105 <AlexM1105ur@gmail.com>
2019-11-29 02:53:22 +0200

    feat (core): add mat-snack-bar module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatSnackBarModule } from '@angular/material';
+
 import { NgxsStoreModule } from '../ngxs/ngxs.module';
 import { INTERCEPTORS } from './interceptors';
 import { VALIDATION, VALIDATION_VALUE } from './constants/validation.const';
@@ -14,6 +16,8 @@ import { VALIDATION, VALIDATION_VALUE } from './constants/validation.const';
     BrowserAnimationsModule,
     NgxsStoreModule,
     HttpClientModule,
+    MatSnackBarModule,
+
   ],
   providers: [
     INTERCEPTORS,
